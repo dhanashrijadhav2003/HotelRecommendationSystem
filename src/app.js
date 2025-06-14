aea2719af1ad403044d5aa4a2f87eb1a6e16c5c5
dhanashrijadhav2003 <jadhavdhanashri969@gmail.com>
2025-06-14 18:24:52 +0530

    change in app
    change by dhanashri

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ let dotenv=require("dotenv");
 let bodyParser=require("body-parser");
 let db=require("./config/db.js");
 let router=require("./routes/routes.js");
+let hotelRoutes=require("./routes/hotel.routes");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -17,6 +18,8 @@ app.use(cors());
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(cookieParser());
+
+app.use("/api,hotelRoutes");
 
 
 dotenv.config();
